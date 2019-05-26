b0666c61286c6b3bf08f239a0e33e05f17608db6
Vlad <vladdemidik@gmail.com>
2019-05-26 15:07:26 +0300

    menu items

--- resources/views/parts/menu.blade.php
+++ resources/views/parts/menu.blade.php
@@ -1,19 +1,31 @@
 <nav class="navbar navbar-expand-lg navbar-light header__container-fluid d-flex justify-content-sm-center mb-4">
     <div class="container vnl__container">
-
+        @php($menuItems = \App\FrontendMenuItem::all())
         <div class="d-none d-lg-block w-100">
             <ul class="navbar-nav w-100">
 
-                <li class="nav-item col-auto p-0">
-                    <a href="{{route('main')}}" class="btn btn-primary p-0 w-100" data-toggle="tooltip" data-placement="bottom" title="Главная страница">
-                        <i class="fas fa-home fa-2x"></i>
-                    </a>
-                </li>
+                @if (Route::currentRouteName() == 'choice')
+                    <li class="nav-item col-auto p-0">
+                        <a href="{{route('main')}}" class="btn btn-primary p-0 w-100" data-toggle="tooltip"
+                           data-placement="bottom" title="Главная страница">
+                            <i class="fas fa-home fa-2x"></i>
+                        </a>
+                    </li>
+                @else
+                    <li class="nav-item col-auto p-0">
+                        <a href="{{route('choice')}}" class="btn btn-primary p-0 w-100" data-toggle="tooltip"
+                           data-placement="bottom" title="Страница выбора">
+                            <i class="fas fa-arrow-alt-circle-left fa-2x"></i>
+                        </a>
+                    </li>
+                @endif
 
                 <li class="nav-item col p-0">
-                    <a href="{{ route('showcase.stuffing') }}" class="btn btn-primary p-0 w-100">
-                        <h2 class="d-none d-sm-inline-block header__item">Начинки<br>&nbsp;</h2>
 
+                    <a href="{{ $menuItems[0]->link }}" class="btn btn-primary p-0 w-100">
+                        <h2 class="d-none d-sm-inline-block header__item">
+                            {{ $menuItems[0]->title }}<br>&nbsp;
+                        </h2>
                     </a>
                 </li>
 
@@ -22,9 +34,14 @@
                 </li>
 
                 <li class="nav-item col p-0">
-                    <a href="{{ route('gallery.cakes') }}" class="btn btn-primary p-0 w-100">
-                        <span class="d-none d-lg-inline-block header__item"><h2 style="line-height: 15px">Торты</h2><h4>Галлерея</h4></span>
-                        <h3 class="d-lg-none">Галлерея тортов</h3>
+                    <a href="{{ $menuItems[1]->link }}" class="btn btn-primary p-0 w-100">
+                        <span class="d-none d-lg-inline-block header__item">
+                            <h2 style="line-height: 15px">
+                                {{ $menuItems[1]->title }}
+                            </h2>
+                            <h4>{{ $menuItems[1]->caption }}</h4>
+                        </span>
+                        <h3 class="d-lg-none">{{ $menuItems[1]->mobileTitle }}</h3>
                     </a>
                 </li>
 
@@ -34,7 +51,9 @@
         <a class="navbar-brand col-auto mx-auto p-0" href="{{ route('main') }}">
             <i class="icon icon__xl icon__logo_full"></i>
         </a>
-        <button class="navbar-toggler position-absolute" style="top:40px" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler position-absolute" style="top:40px" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -57,23 +76,30 @@
                 </li>
 
                 <li class="nav-item col-lg p-0">
-                    <a href="{{ route('gallery.deserts') }}" class="btn btn-primary p-0 w-100 text-left text-lg-center">
-                        <span class="d-none d-lg-inline-block header__item"><h2 style="line-height: 15px">Десерты</h2><h4>Галлерея</h4></span>
-                        <h3 class="d-lg-none">Галлерея десертов</h3>
+                    <a href="{{ $menuItems[2]->link }}" class="btn btn-primary p-0 w-100 text-left text-lg-center">
+                        <span class="d-none d-lg-inline-block header__item">
+                            <h2 style="line-height: 15px">{{ $menuItems[2]->title }}</h2>
+                            <h4>{{ $menuItems[2]->caption }}</h4>
+                        </span>
+                        <h3 class="d-lg-none">{{ $menuItems[2]->mobileTitle }}</h3>
                     </a>
                 </li>
+
                 <li class="nav-item col-auto d-none d-lg-block">
                     <span class=" icon icon__delimiter"></span>
                 </li>
                 <li class="nav-item col-lg p-0">
-                    <a href="{{ route('showcase.deserts') }}" class="btn btn-primary p-0 w-100 text-left text-lg-center">
-                        <h2 class="d-none d-lg-inline-block header__item">Десерты<br></h2>
-                        <h3 class="d-lg-none">Десерты</h3>
+                    <a href="{{ $menuItems[3]->link }}" class="btn btn-primary p-0 w-100 text-left text-lg-center">
+                        <h2 class="d-none d-lg-inline-block header__item">{{ $menuItems[3]->title }}<br></h2>
+                        <h3 class="d-lg-none">{{ $menuItems[3]->mobileTitle }}</h3>
                     </a>
                 </li>
+
                 <li class="nav-item col-lg-auto p-0">
-                    <a href="#faq-modal-open" class="btn btn-primary p-0 w-100 text-left text-lg-center" data-toggle="tooltip" data-placement="bottom" title="Вопросы/Ответы">
-                        <i class="d-none d-lg-inline-block fas fa-question-circle fa-2x"></i> <h3 class="d-lg-none">Вопросы и ответы</h3>
+                    <a href="#faq-modal-open" class="btn btn-primary p-0 w-100 text-left text-lg-center"
+                       data-toggle="tooltip" data-placement="bottom" title="Вопросы/Ответы">
+                        <i class="d-none d-lg-inline-block fas fa-question-circle fa-2x"></i>
+                        <h3 class="d-lg-none">Вопросы и ответы</h3>
                     </a>
                 </li>
             </ul>
